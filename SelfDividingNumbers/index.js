fc46081d8eb218add3fbd6ac6c80105c4e7d3199
seongwoon hong <saytoyou91@gmail.com>
2018-01-26 08:11:28 -0500

    Self Dividing Numbers

---
+++ SelfDividingNumbers/index.js
@@ -0,0 +1,38 @@
+/*
+A self-dividing number is a number that is divisible by every digit it contains.
+
+For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+Also, a self-dividing number is not allowed to contain the digit zero.
+
+Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+Example 1:
+Input:
+left = 1, right = 22
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+Note:
+
+The boundaries of each input argument are 1 <= left <= right <= 10000.
+*/
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    if (1 <= left && left <= right && right <= 10000) {
+      let result = [];
+      for (let i = left; i <= right; i++) {
+        let str = '';
+        str += i;
+        let isSelfDividing = str.split('').every((num) => {
+          return i % num === 0;
+        })
+        isSelfDividing && result.push(i);
+      }
+      return result;
+    } else {
+      return null;
+    }
+};
