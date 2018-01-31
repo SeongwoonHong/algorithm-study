fec84da2defded7af1f6212678639e1cfe340aff
seongwoon hong <saytoyou91@gmail.com>
2018-01-31 00:07:16 -0500

    Trim Binary Search Tree

--- TrimBinarySearchTree/index.js
+++ TrimBinarySearchTree/index.js
@@ -51,11 +51,14 @@ Output:
  * @return {TreeNode}
  */
 var trimBST = function(root, L, R) {
-  if (root === null) return root;
-  if (root.left < L) return trimBST(root.right, L, R);
-  if (root.right < R) return trimBST(root.left, L, R);
-  let t = TreeNode(root);
+  if (root === null) return null;
+  if (root.val < L) {
+    return trimBST(root.right, L, R);
+  }
+  if (root.val > R) {
+    return trimBST(root.left, L, R);
+  }
   root.left = trimBST(root.left, L, R);
   root.right = trimBST(root.right, L, R);
-  return t;
+  return root;
 };
