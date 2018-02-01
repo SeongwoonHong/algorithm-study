c0a8ea5e95343187b061a6029402bad9b15fcad6
seongwoon hong <saytoyou91@gmail.com>
2018-02-01 00:38:00 -0500

    Sum Of Two

---
+++ SumOfTwo/index.js
@@ -0,0 +1,14 @@
+var sumOfTwo = function(arr, sum) {
+  let hashTable = {};
+  let result = [];
+  let length = arr.length;
+  for (let i = 0; i < length; i++) {
+    let otherpair =  sum - arr[i];
+    hashTable[arr[i]] = 1;
+
+    if (hashTable[otherpair]) { 
+      result.push([arr[i], otherpair]);
+    }
+  }
+  return result;
+}
