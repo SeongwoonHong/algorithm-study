29f6a233b024e11cd7a359201fb1b2abb0227388
seongwoon hong <saytoyou91@gmail.com>
2018-02-13 08:57:40 -0500

    Maxchar + Sorting + Events

--- sorting/index.js
+++ sorting/index.js
@@ -2,19 +2,41 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j+1]) {
+        const lesser = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[indexOfMin] > arr[j]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
-
+// will implement this later
 }
 
 function merge(left, right) {
-
+// will implement this later
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
