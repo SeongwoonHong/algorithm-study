c0cb7a3aa757009bd86e0163e9fa104f6430e3a5
seongwoon hong <saytoyou91@gmail.com>
2018-02-16 11:57:11 -0500

    Implemented merge sort

--- sorting/index.js
+++ sorting/index.js
@@ -32,11 +32,28 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-// will implement this later
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-// will implement this later
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
 }
 
 function insertionSort(arr) {
