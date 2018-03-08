f2cc94ac78e3d26e6b9f704af6f903ec7137ac12
seongwoon hong <saytoyou91@gmail.com>
2018-03-07 23:29:53 -0500

    Reverse string that has tabs

---
+++ ReverseTab/index.js
@@ -0,0 +1,26 @@
+function reverseAll(str) {
+  let temp = '';
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      temp += str[i];
+    } else {
+      if (temp.length > 0) {
+        result.push(reverse(temp));
+        temp = [];
+      }
+    }
+    if (i === str.length - 1 && temp.length > 0) {
+      result.push(reverse(temp));
+    }
+  }
+  return result;
+}
+
+function reverse(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+  return result;
+}
