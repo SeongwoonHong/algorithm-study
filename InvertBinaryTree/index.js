016248f3dd42a277248a08b24fa969cf172405d7
seongwoon hong <saytoyou91@gmail.com>
2020-07-12 16:48:41 -0400

    Inverty Binary Tree

---
+++ InvertBinaryTree/index.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (!root) {
+      return root;
+  }
+  
+  let temp = root.left;
+  root.left = invertTree(root.right);
+  root.right = invertTree(temp);
+  
+  return root;
+};
